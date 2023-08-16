4b15102a006f367a74d24f2e1fc3f72eeeb62e02
Asma <asmaelghoul13@gmail.com>
2023-08-16 04:47:03 +0300

    Feature/Adding-FirstAidKit
    commit cc8761a28599c87255b5cf8bf38f2ea96cecd70a
    Author: Asma <asmaelghoul13@gmail.com>
    Date:   Wed Aug 16 04:46:21 2023 +0300
    
        adding First Aid Kit

--- app/src/main/java/myproject1/ash1/emergency/ui/FirstAidKit.kt
+++ app/src/main/java/myproject1/ash1/emergency/ui/FirstAidKit.kt
@@ -5,19 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 import myproject1.ash1.emergency.R
+import myproject1.ash1.emergency.databinding.FragmentEmergencyCallBinding
+import myproject1.ash1.emergency.databinding.FragmentFirstAidKitBinding
 
 
 class FirstAidKit : Fragment() {
 
+    lateinit var binding: FragmentFirstAidKitBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_first_aid_kit, container, false)
+        binding= FragmentFirstAidKitBinding.inflate(layoutInflater)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val viewPager=activity?.findViewById<ViewPager2>(R.id.viewPager)
+        binding.firstAidKitIcSkip.setOnClickListener{
+            viewPager?.currentItem=2
+        }
+        binding.nextBtn.setOnClickListener{
+            viewPager?.currentItem=0
+        }
+    }
 
 }
